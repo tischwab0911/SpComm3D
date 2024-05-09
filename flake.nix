7ffb30bfa2b7c7d3e9739fa05a557c641a2588c2
Louis Hurschler <louis.hurschler@gmx.ch>
2024-05-09 20:04:27 +0200

    cmake and make works but not for benchBoth and serialSim because of old coo_mtx class

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "HPC Lab";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShell.${system} = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+        cmake
+        clang-tools
+        mpich
+        ];
+        buildInputs = with pkgs; [ 
+        ];
+
+        CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+      };
+    };
+}
